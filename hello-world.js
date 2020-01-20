e93fd7e80a6e7f2bb06c66c97fd34e7033ef724b
unknown <you@example.com>
2020-01-20 14:53:54 +0100

    first commit

---
+++ hello-world.js
@@ -0,0 +1,16 @@
+export const hello =  () => "Hello, World!";
+
+/**
+ * function hello(){
+ *  return "Hello World!";
+ * }
+ * 
+ */
+
+/*const hello =  async() => {
+    await new Promise(res => setTimeout(res, 1000));
+   return "Hello, World!";
+  };
+   hello();
+    */
+
